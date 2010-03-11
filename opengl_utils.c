ef9e39ef567178be18e8631ee1cb8b3747f82718
root <root@debian.config>
2010-03-11 13:30:41 +0000

    move helper functions

--- opengl_utils.c
+++ opengl_utils.c
@@ -675,3 +675,298 @@ void removeUnwantedExtensions(char* ret)
   }
   free(toBeRemoved);
 }
+
+static int compute_arg_length(FILE* err_file, int func_number, int arg_i, long* args)
+{
+  Signature* signature = (Signature*)tab_opengl_calls[func_number];
+  int* args_type = signature->args_type;
+
+  switch (func_number)
+  {
+    case glProgramNamedParameter4fNV_func:
+    case glProgramNamedParameter4dNV_func:
+    case glProgramNamedParameter4fvNV_func:
+    case glProgramNamedParameter4dvNV_func:
+    case glGetProgramNamedParameterfvNV_func:
+    case glGetProgramNamedParameterdvNV_func:
+      if (arg_i == 2)
+        return 1 * args[arg_i-1] * tab_args_type_length[args_type[arg_i]];
+      break;
+
+    case glProgramStringARB_func:
+    case glLoadProgramNV_func:
+    case glGenProgramsNV_func:
+    case glDeleteProgramsNV_func:
+    case glGenProgramsARB_func:
+    case glDeleteProgramsARB_func:
+    case glRequestResidentProgramsNV_func:
+    case glDrawBuffers_func:
+    case glDrawBuffersARB_func:
+    case glDrawBuffersATI_func:
+    case glDeleteBuffers_func:
+    case glDeleteBuffersARB_func:
+    case glDeleteTextures_func:
+    case glDeleteTexturesEXT_func:
+    case glGenFramebuffersEXT_func:
+    case glDeleteFramebuffersEXT_func:
+    case glGenRenderbuffersEXT_func:
+    case glDeleteRenderbuffersEXT_func:
+    case glGenQueries_func:
+    case glGenQueriesARB_func:
+    case glDeleteQueries_func:
+    case glDeleteQueriesARB_func:
+    case glGenOcclusionQueriesNV_func:
+    case glDeleteOcclusionQueriesNV_func:
+    case glGenFencesNV_func:
+    case glDeleteFencesNV_func:
+    case glUniform1fv_func:
+    case glUniform1iv_func:
+    case glUniform1fvARB_func:
+    case glUniform1ivARB_func:
+    case glUniform1uivEXT_func:
+    case glVertexAttribs1dvNV_func:
+    case glVertexAttribs1fvNV_func:
+    case glVertexAttribs1svNV_func:
+    case glVertexAttribs1hvNV_func:
+    case glWeightbvARB_func:
+    case glWeightsvARB_func:
+    case glWeightivARB_func:
+    case glWeightfvARB_func:
+    case glWeightdvARB_func:
+    case glWeightubvARB_func:
+    case glWeightusvARB_func:
+    case glWeightuivARB_func:
+    case glPixelMapfv_func:
+    case glPixelMapuiv_func:
+    case glPixelMapusv_func:
+    case glProgramBufferParametersfvNV_func:
+    case glProgramBufferParametersIivNV_func:
+    case glProgramBufferParametersIuivNV_func:
+    case glTransformFeedbackAttribsNV_func:
+    case glTransformFeedbackVaryingsNV_func:
+      if (arg_i == signature->nb_args - 1)
+        return 1 * args[arg_i-1] * tab_args_type_length[args_type[arg_i]];
+      break;
+
+    case glUniform2fv_func:
+    case glUniform2iv_func:
+    case glUniform2fvARB_func:
+    case glUniform2ivARB_func:
+    case glUniform2uivEXT_func:
+    case glVertexAttribs2dvNV_func:
+    case glVertexAttribs2fvNV_func:
+    case glVertexAttribs2svNV_func:
+    case glVertexAttribs2hvNV_func:
+    case glDetailTexFuncSGIS_func:
+    case glSharpenTexFuncSGIS_func:
+      if (arg_i == signature->nb_args - 1)
+        return 2 * args[arg_i-1] * tab_args_type_length[args_type[arg_i]];
+      break;
+
+    case glUniform3fv_func:
+    case glUniform3iv_func:
+    case glUniform3fvARB_func:
+    case glUniform3ivARB_func:
+    case glUniform3uivEXT_func:
+    case glVertexAttribs3dvNV_func:
+    case glVertexAttribs3fvNV_func:
+    case glVertexAttribs3svNV_func:
+    case glVertexAttribs3hvNV_func:
+      if (arg_i == signature->nb_args - 1)
+        return 3 * args[arg_i-1] * tab_args_type_length[args_type[arg_i]];
+      break;
+
+    case glUniform4fv_func:
+    case glUniform4iv_func:
+    case glUniform4fvARB_func:
+    case glUniform4ivARB_func:
+    case glUniform4uivEXT_func:
+    case glVertexAttribs4dvNV_func:
+    case glVertexAttribs4fvNV_func:
+    case glVertexAttribs4svNV_func:
+    case glVertexAttribs4hvNV_func:
+    case glVertexAttribs4ubvNV_func:
+    case glProgramParameters4fvNV_func:
+    case glProgramParameters4dvNV_func:
+    case glProgramLocalParameters4fvEXT_func:
+    case glProgramEnvParameters4fvEXT_func:
+    case glProgramLocalParametersI4ivNV_func:
+    case glProgramLocalParametersI4uivNV_func:
+    case glProgramEnvParametersI4ivNV_func:
+    case glProgramEnvParametersI4uivNV_func:
+      if (arg_i == signature->nb_args - 1)
+        return 4 * args[arg_i-1] * tab_args_type_length[args_type[arg_i]];
+      break;
+
+    case glPrioritizeTextures_func:
+    case glPrioritizeTexturesEXT_func:
+    case glAreProgramsResidentNV_func:
+    case glAreTexturesResident_func:
+    case glAreTexturesResidentEXT_func:
+      if (arg_i == 1 || arg_i == 2)
+        return args[0] * tab_args_type_length[args_type[arg_i]];
+      break;
+
+    case glLightfv_func:
+    case glLightiv_func:
+    case glGetLightfv_func:
+    case glGetLightiv_func:
+    case glFragmentLightfvSGIX_func:
+    case glFragmentLightivSGIX_func:
+    case glGetFragmentLightfvSGIX_func:
+    case glGetFragmentLightivSGIX_func:
+      if (arg_i == signature->nb_args - 1)
+        return __glLight_size(err_file, args[arg_i-1]) * tab_args_type_length[args_type[arg_i]];
+      break;
+
+    case glLightModelfv_func:
+    case glLightModeliv_func:
+      if (arg_i == signature->nb_args - 1)
+        return ((args[arg_i-1] == GL_LIGHT_MODEL_AMBIENT) ? 4 : 1) * tab_args_type_length[args_type[arg_i]];
+      break;
+
+    case glFragmentLightModelfvSGIX_func:
+    case glFragmentLightModelivSGIX_func:
+      if (arg_i == signature->nb_args - 1)
+        return ((args[arg_i-1] == GL_FRAGMENT_LIGHT_MODEL_AMBIENT_SGIX) ? 4 : 1) * tab_args_type_length[args_type[arg_i]];
+      break;
+
+    case glMaterialfv_func:
+    case glMaterialiv_func:
+    case glGetMaterialfv_func:
+    case glGetMaterialiv_func:
+    case glFragmentMaterialfvSGIX_func:
+    case glFragmentMaterialivSGIX_func:
+    case glGetFragmentMaterialfvSGIX_func:
+    case glGetFragmentMaterialivSGIX_func:
+      if (arg_i == signature->nb_args - 1)
+        return __glMaterial_size(err_file, args[arg_i-1]) * tab_args_type_length[args_type[arg_i]];
+      break;
+
+    case glTexParameterfv_func:
+    case glTexParameteriv_func:
+    case glGetTexParameterfv_func:
+    case glGetTexParameteriv_func:
+    case glTexParameterIivEXT_func:
+    case glTexParameterIuivEXT_func:
+    case glGetTexParameterIivEXT_func:
+    case glGetTexParameterIuivEXT_func:
+      if (arg_i == signature->nb_args - 1)
+        return __glTexParameter_size(err_file, args[arg_i-1]) * tab_args_type_length[args_type[arg_i]];
+      break;
+
+    case glFogiv_func:
+    case glFogfv_func:
+      if (arg_i == signature->nb_args - 1)
+        return ((args[arg_i-1] == GL_FOG_COLOR) ? 4 : 1) * tab_args_type_length[args_type[arg_i]];
+      break;
+
+    case glTexGendv_func:
+    case glTexGenfv_func:
+    case glTexGeniv_func:
+    case glGetTexGendv_func:
+    case glGetTexGenfv_func:
+    case glGetTexGeniv_func:
+      if (arg_i == signature->nb_args - 1)
+        return ((args[arg_i-1] == GL_TEXTURE_GEN_MODE) ? 1 : 4) * tab_args_type_length[args_type[arg_i]];
+      break;
+
+    case glTexEnvfv_func:
+    case glTexEnviv_func:
+    case glGetTexEnvfv_func:
+    case glGetTexEnviv_func:
+      if (arg_i == signature->nb_args - 1)
+        return ((args[arg_i-1] == GL_TEXTURE_ENV_MODE) ? 1 : 4) * tab_args_type_length[args_type[arg_i]];
+      break;
+
+    case glConvolutionParameterfv_func:
+    case glConvolutionParameteriv_func:
+    case glGetConvolutionParameterfv_func:
+    case glGetConvolutionParameteriv_func:
+    case glConvolutionParameterfvEXT_func:
+    case glConvolutionParameterivEXT_func:
+    case glGetConvolutionParameterfvEXT_func:
+    case glGetConvolutionParameterivEXT_func:
+      if (arg_i == signature->nb_args - 1)
+        return ((args[arg_i-1] == GL_CONVOLUTION_BORDER_COLOR ||
+                 args[arg_i-1] == GL_CONVOLUTION_FILTER_SCALE ||
+                 args[arg_i-1] == GL_CONVOLUTION_FILTER_BIAS) ? 4 : 1) * tab_args_type_length[args_type[arg_i]];
+      break;
+
+    case glGetVertexAttribfvARB_func:
+    case glGetVertexAttribfvNV_func:
+    case glGetVertexAttribfv_func:
+    case glGetVertexAttribdvARB_func:
+    case glGetVertexAttribdvNV_func:
+    case glGetVertexAttribdv_func:
+    case glGetVertexAttribivARB_func:
+    case glGetVertexAttribivNV_func:
+    case glGetVertexAttribiv_func:
+    case glGetVertexAttribIivEXT_func:
+    case glGetVertexAttribIuivEXT_func:
+      if (arg_i == signature->nb_args - 1)
+        return ((args[arg_i-1] == GL_CURRENT_VERTEX_ATTRIB_ARB) ? 4 : 1) * tab_args_type_length[args_type[arg_i]];
+      break;
+
+
+    case glPointParameterfv_func:
+    case glPointParameterfvEXT_func:
+    case glPointParameterfvARB_func:
+    case glPointParameterfvSGIS_func:
+    case glPointParameteriv_func:
+    case glPointParameterivEXT_func:
+      if (arg_i == signature->nb_args - 1)
+        return ((args[arg_i-1] == GL_POINT_DISTANCE_ATTENUATION) ? 3 : 1) * tab_args_type_length[args_type[arg_i]];
+      break;
+
+    case glUniformMatrix2fv_func:
+    case glUniformMatrix2fvARB_func:
+      if (arg_i == signature->nb_args - 1)
+        return 2 * 2 * args[1] * tab_args_type_length[args_type[arg_i]];
+      break;
+
+    case glUniformMatrix3fv_func:
+    case glUniformMatrix3fvARB_func:
+      if (arg_i == signature->nb_args - 1)
+        return 3 * 3 * args[1] * tab_args_type_length[args_type[arg_i]];
+      break;
+
+    case glUniformMatrix4fv_func:
+    case glUniformMatrix4fvARB_func:
+      if (arg_i == signature->nb_args - 1)
+        return 4 * 4 * args[1] * tab_args_type_length[args_type[arg_i]];
+      break;
+
+    case glUniformMatrix2x3fv_func:
+    case glUniformMatrix3x2fv_func:
+      if (arg_i == signature->nb_args - 1)
+        return 2 * 3 * args[1] * tab_args_type_length[args_type[arg_i]];
+      break;
+
+    case glUniformMatrix2x4fv_func:
+    case glUniformMatrix4x2fv_func:
+      if (arg_i == signature->nb_args - 1)
+        return 2 * 4 * args[1] * tab_args_type_length[args_type[arg_i]];
+      break;
+
+    case glUniformMatrix3x4fv_func:
+    case glUniformMatrix4x3fv_func:
+      if (arg_i == signature->nb_args - 1)
+        return 3 * 4 * args[1] * tab_args_type_length[args_type[arg_i]];
+      break;
+
+    case glSpriteParameterivSGIX_func:
+    case glSpriteParameterfvSGIX_func:
+      if  (arg_i == signature->nb_args - 1)
+        return ((args[arg_i-1] == GL_SPRITE_MODE_SGIX) ? 1 : 3) * tab_args_type_length[args_type[arg_i]];
+      break;
+
+    default:
+      break;
+  }
+
+  fprintf(err_file, "invalid combination for compute_arg_length : func_number=%d, arg_i=%d\n", func_number, arg_i);
+  return 0;
+}
+
+
--- opengl_utils.h
+++ opengl_utils.h
@@ -44,5 +44,6 @@ extern void delete_consecutive_values(RangeAllocator* range, unsigned int first,
 extern unsigned int calc_checksum(const void* _ptr, int length, unsigned int seed);
 extern void display_gl_call(FILE* f, int func_number, long* args, int* args_size);
 extern void removeUnwantedExtensions(char* ret);
+extern int compute_arg_length(FILE* err_file, int func_number, int arg_i, long* args);
 
 #endif
